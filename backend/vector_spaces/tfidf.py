aef0744dacb3ab244ab8adff35d5737826d7de7c
Long Nguyen <long.pollehn@gmail.com>
2023-06-30 23:07:39 +0200

    Fitting works

--- backend/vector_spaces/tfidf.py
+++ backend/vector_spaces/tfidf.py
@@ -6,7 +6,7 @@ import json
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tqdm import tqdm
 
-from backend.streamers import DocumentTitleStringStreamer, DocumentStreamer, DocumentBodyStringStreamer
+from backend.streamers import *
 from crawler import utils
 from crawler.sql_models.base import BaseModel
 from crawler.sql_models.document import Document
@@ -58,50 +58,62 @@ def train():
     }
     try:
         vectorizers["title"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        LOG.info("Fitted title vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting title vectorizer {e}")
     try:
-        vectorizers["meta_description"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["meta_description"].fit(DocumentMetaDescriptionStringStreamer())
+        LOG.info("Fitted meta_description vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting meta_description vectorizer {e}")
     try:
-        vectorizers["meta_keywords"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["meta_keywords"].fit(DocumentMetaKeywordsStringStreamer())
+        LOG.info("Fitted meta_keywords vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting meta_keywords vectorizer {e}")
     try:
-        vectorizers["meta_author"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["meta_author"].fit(DocumentMetaAuthorStringStreamer())
+        LOG.info("Fitted meta_author vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting meta_author vectorizer {e}")
     try:
-        vectorizers["h1"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["h1"].fit(DocumentH1StringStreamer())
+        LOG.info("Fitted h1 vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting h1 vectorizer {e}")
     try:
-        vectorizers["h2"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["h2"].fit(DocumentH2StringStreamer())
+        LOG.info("Fitted h2 vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting h2 vectorizer {e}")
     try:
-        vectorizers["h3"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["h3"].fit(DocumentH3StringStreamer())
+        LOG.info("Fitted h3 vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting h3 vectorizer {e}")
     try:
-        vectorizers["h4"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["h4"].fit(DocumentH4StringStreamer())
+        LOG.info("Fitted h4 vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting h4 vectorizer {e}")
     try:
-        vectorizers["h5"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["h5"].fit(DocumentH5StringStreamer())
+        LOG.info("Fitted h5 vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting h5 vectorizer {e}")
     try:
-        vectorizers["h6"].fit(DocumentTitleStringStreamer())
-    except:
-        pass
+        vectorizers["h6"].fit(DocumentH6StringStreamer())
+        LOG.info("Fitted h6 vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting h6 vectorizer {e}")
     try:
         vectorizers["body"].fit(DocumentBodyStringStreamer())
-    except:
-        pass
+        LOG.info("Fitted body vectorizer")
+    except Exception as e:
+        LOG.error(f"Error while fitting body vectorizer {e}")
     utils.io.write_pickle_file(vectorizers, TFIDF_VECTORIZER_FILE)
     LOG.info(f"Wrote TF-IDF file to {TFIDF_VECTORIZER_FILE}")
+
 
 def transform():
     LOG.info("Start vectorize database's documents with the global TF-IDF to database")
@@ -111,49 +123,51 @@ def transform():
             tfidf, created = Tfidf.get_or_create(id=document.id)
             try:
                 tfidf.title = vectorizers["title"].transform([" ".join(document.title_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming title {e}")
             try:
-                tfidf.meta_description = vectorizers["meta_description"].transform([" ".join(document.meta_description_tokens)])[
+                tfidf.meta_description = \
+                vectorizers["meta_description"].transform([" ".join(document.meta_description_tokens)])[
                     0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming meta_description {e}")
             try:
-                tfidf.meta_keywords = vectorizers["meta_keywords"].transform([" ".join(document.meta_keywords_tokens)])[0]
-            except:
-                pass
+                tfidf.meta_keywords = vectorizers["meta_keywords"].transform([" ".join(document.meta_keywords_tokens)])[
+                    0]
+            except Exception as e:
+                LOG.error(f"Error while transforming meta_keywords {e}")
             try:
                 tfidf.meta_author = vectorizers["meta_author"].transform([" ".join(document.meta_author_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming meta_author {e}")
             try:
                 tfidf.h1 = vectorizers["h1"].transform([" ".join(document.h1_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming h1 {e}")
             try:
                 tfidf.h2 = vectorizers["h2"].transform([" ".join(document.h2_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming h2 {e}")
             try:
                 tfidf.h3 = vectorizers["h3"].transform([" ".join(document.h3_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming h3 {e}")
             try:
                 tfidf.h4 = vectorizers["h4"].transform([" ".join(document.h4_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming h4 {e}")
             try:
                 tfidf.h5 = vectorizers["h5"].transform([" ".join(document.h5_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming h5 {e}")
             try:
                 tfidf.h6 = vectorizers["h6"].transform([" ".join(document.h6_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming h6 {e}")
             try:
                 tfidf.body = vectorizers["body"].transform([" ".join(document.body_tokens)])[0]
-            except:
-                pass
+            except Exception as e:
+                LOG.error(f"Error while transforming body {e}")
             tfidf.save()
         except Exception as e:
             LOG.error(f"Can not write results of global TF-IDF of one entry to database: {e}")
