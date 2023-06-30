69938aee09d624f058a8baebc774003b6eab760a
Philipp Alber <info@philippalber.de>
2023-06-30 19:52:06 +0200

    Optimize CI

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,4 +4,4 @@ set -e
 python3 -m pylint --rcfile=.pylintrc crawler
 python3 -m pylint --rcfile=.pylintrc backend
 python3 -m pylint --rcfile=.pylintrc scripts
-python3 -m pytest --junitxml="${TEST_REPORT_FILE:./pytest-results.xml}"
+python3 -m pytest --junitxml=pytest-results.xml
