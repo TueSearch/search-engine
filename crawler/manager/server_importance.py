2c830f1dee2e24352d5a97197012bde54b75a8e2
Long Nguyen <long.pollehn@gmail.com>
2023-07-04 18:48:11 +0200

    Relevant factor

--- crawler/manager/server_importance.py
+++ crawler/manager/server_importance.py
@@ -7,6 +7,9 @@ This could also be done at client side but I decided not to to save time.
 """
 from crawler.sql_models.server import Server
 
+SUCCESS_FACTOR = 3
+RELEVANT_FACTOR = 50
+THRESHOLD = 0.1
 
 def server_importance(server_id: int):
     """
@@ -15,7 +18,13 @@ def server_importance(server_id: int):
     priority = 0
     server = Server.select().where(Server.id == server_id).get()
     priority += min(5, server.page_rank * 5)
-    if server.total_jobs > 0:
-        priority += min(1, (server.success_jobs / server.total_done_jobs) * 1)
-        priority += min(10, (server.relevant_documents / server.total_done_jobs) * 10)
+    if server.total_done_jobs > 0:
+        success_ratio = server.success_jobs / server.total_done_jobs
+        relevant_ratio = server.relevant_documents / server.total_done_jobs
+        priority += SUCCESS_FACTOR * (success_ratio - THRESHOLD) ** 3
+        priority += RELEVANT_FACTOR * (relevant_ratio - THRESHOLD) ** 3
+        if success_ratio < THRESHOLD:
+            priority -= 2
+        if relevant_ratio < THRESHOLD:
+            priority -= 2
     return priority
