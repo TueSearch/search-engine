eea3e1a54bc5feac20d8046942326a69d656a5c6
Long Nguyen <long.pollehn@gmail.com>
2023-07-04 21:08:22 +0200

    Removed trigger

--- crawler/manager/main.py
+++ crawler/manager/main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
 from crawler.manager.server_importance import server_importance
-from crawler.manager.priority_queue import PriorityQueue
+from crawler.manager.priority_queue import PriorityQueue, file_lock
 from crawler.worker.url_relevance import URL
 from crawler.sql_models.base import connect_to_database, dotdict
 from crawler.sql_models.document import Document
@@ -88,6 +88,7 @@ def mark_job_as_fail(job_id):
 
 @app.route('/save_crawling_results/<int:parent_job_id>', methods=['POST'])
 @check_password
+@file_lock
 def save_crawling_results(parent_job_id):
     """
     Save the crawling results.
@@ -102,6 +103,12 @@ def save_crawling_results(parent_job_id):
     new_document.save()
     LOG.info(f"Created document {new_document.id}")
 
+    # Update server's relevance
+    if new_document_server.relevant:
+        new_document_server = new_document.job.server
+        new_document_server.relevant_documents += 1
+        new_document_server.save()
+        
     # Create new servers from URLs if new servers were met while crawling.
     new_jobs_links = list(set(URL(job.url) for job in new_jobs))
     new_links_to_server_id = Server.create_servers_and_return_ids(new_jobs_links)
