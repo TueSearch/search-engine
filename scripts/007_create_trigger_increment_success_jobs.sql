e3609bade7ac132c30e176ae08ea089a41513802
Long Nguyen <long.pollehn@gmail.com>
2023-07-04 23:03:48 +0200

    Total done jobs before success jobs

--- scripts/007_create_trigger_increment_success_jobs.sql
+++ scripts/007_create_trigger_increment_success_jobs.sql
@@ -4,13 +4,13 @@ CREATE TRIGGER increment_success_jobs
     FOR EACH ROW
 BEGIN
     IF NEW.done != OLD.done THEN
+        UPDATE servers
+        set total_done_jobs = total_done_jobs + 1
+        WHERE id = NEW.server_id;
         IF NEW.success = TRUE THEN -- Only an approximation, but good enough.
             UPDATE servers
             SET success_jobs = success_jobs + 1
             WHERE id = NEW.server_id;
         END IF;
-        UPDATE servers
-        set total_done_jobs = total_done_jobs + 1
-        WHERE id = NEW.server_id;
     END IF;
 END;
