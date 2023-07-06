1d75b337c7f53155c113cc7b9ccbf5bcb9d7dcf1
Long Nguyen <long.pollehn@gmail.com>
2023-07-06 11:32:36 +0200

    Updated index and metrics used wrong script

--- scripts/update_index.sh
+++ scripts/update_index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Run on host
 
-./script_teardown.sh
+./scripts/tear_down.sh
 docker-compose up --build -d mysql
 docker-compose up --build --exit-code-from update_index update_index
