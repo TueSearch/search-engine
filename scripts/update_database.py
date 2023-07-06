a343eb5d5dd50ded91d6d8d0c030ddbd121daff2
Long Nguyen <long.pollehn@gmail.com>
2023-07-06 08:00:02 +0200

    Added updated number

--- scripts/update_database.py
+++ scripts/update_database.py
@@ -15,13 +15,15 @@ def update_priority_of_jobs_in_database():
     """
     Updates the priority of all jobs in the database.
     """
+    updated = 0
     LOG.info(f"Starting to update priority of jobs in database.")
     for job in Job.select().where(Job.done == False):
         try:
             before = job.priority
             job.priority = server_importance(job.server_id) + URL(job.url).priority
-            LOG.info("Updating priority of job: " + job.url + " from " + str(before) + " to " + str(job.priority))
+            LOG.info(f"[{updated}]Updating priority of job: " + job.url + " from " + str(before) + " to " + str(job.priority))
             job.save()
+            updated += 1
         except Exception as e:
             LOG.info("Error while updating priority of job: " + str(model_to_dict(job)))
             LOG.info(e)
