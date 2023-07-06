51050340d5e6801911c0573c50da880d05834577
Long Nguyen <long.pollehn@gmail.com>
2023-07-06 11:37:12 +0200

    Fixed minor

--- scripts/update_priority.sh
+++ scripts/update_priority.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Run on host
 
-./script_teardown.sh
+./scripts/tear_down.sh
 docker-compose down
 docker-compose up --build -d mysql
 docker-compose up --build --exit-code-from update_priority update_priority
