9cbc3beac65364114650ffd064f32c8229c58a21
Philipp Alber <info@philippalber.de>
2023-07-18 10:39:49 +0200

    Modify search reslt interface

--- frontend/src/components/SearchResults/SingleDoc.tsx
+++ frontend/src/components/SearchResults/SingleDoc.tsx
@@ -1,11 +1,16 @@
 import { Box, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export interface SearchResultsDocument {
   id: number;
+  scores: number;
   title: string;
+  description: string;
   url: string;
+  is_english_prob: number;
+  is_german_prob: number;
 }
 
 export interface SearchResults {
@@ -17,15 +22,24 @@ export interface SearchResults {
 
 export const SingleDoc = ({ doc }: { doc: SearchResultsDocument }) => {
   return (
-    <Box component="div" sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 1 }}>
-      <Typography variant="h6" data-tid="title">
-        {doc.title}
-      </Typography>
-      <Link to={doc.url} target="_blank" rel="noopener noreferrer">
-        <Typography variant="body1" data-tid="url">
-          {doc.url}
+    <Stack direction={'column'} alignItems={'flex-start'} sx={{ p: 2 }}>
+      <Box component={'div'}>
+        <Typography variant="h6" data-tid="title">
+          {doc.title}
         </Typography>
-      </Link>
-    </Box>
+      </Box>
+      <Box component={'div'}>
+        <Typography variant="body1" data-tid="description">
+          {doc.description}
+        </Typography>
+      </Box>
+      <Box component={'div'}>
+        <Link to={doc.url} target="_blank" rel="noopener noreferrer">
+          <Typography variant="body2" data-tid="url">
+            {doc.url}
+          </Typography>
+        </Link>
+      </Box>
+    </Stack>
   );
 };
